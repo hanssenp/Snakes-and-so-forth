3926fedebc9f2f807ebacfbd2de4a4c7c4d481bd
Peter Hanssen <pjh68@georgetown.edu>
2017-10-13 19:31:46 -0400

    Format changes -- added function main

--- Bayes1.py
+++ Bayes1.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 13 08:52:42 2017
-
-@author: Peter
-"""
-
-# Probability of actually having a disease given a positive test result.
 
 # libraries; N.B. commented packages are not in use in present version.
 import numpy as np
@@ -13,17 +6,20 @@ import matplotlib
 # import scipy
 # import bayespy
 
-# close all plots
-matplotlib.pyplot.close("all")
+# Probability of actually having a disease given a positive test result.
 
-# Bayes' theorem P(A|B) = [P(B|A)P(A)]/[P(B)]
-# P(A) = probability of actually having the disease
-# P(B) = true positive test rate
-A = float(input("Base rate P(A)? "))
-B = float(input("P(B)? "))
+def main():
+    # close all plots
+    matplotlib.pyplot.close("all")
 
-PAB = float((B * A) / ((B * A) + (1 - B) * (1 - A)))
-print("Probability of A, given B: ", PAB)
+    # Bayes' theorem P(A|B) = [P(B|A)P(A)]/[P(B)]
+    # P(A) = probability of actually having the disease
+    # P(B) = true positive test rate
+    A = float(input("Base rate P(A)? "))
+    B = float(input("P(B)? "))
+    
+    PAB = float((B * A) / ((B * A) + (1 - B) * (1 - A)))
+    print("Probability of A, given B: ", PAB)
 
- # end program
- # second change
+if __name__ == '__main__':
+    main()
